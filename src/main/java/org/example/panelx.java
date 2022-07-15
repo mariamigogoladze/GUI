bc045748991f8a5e32238820be66a239aa369fc0
mariami <mariamigogoladze0@gmail.com>
2022-07-15 15:49:51 +0400

    Almost done

--- src/main/java/org/example/panelx.java
+++ src/main/java/org/example/panelx.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 class panelx extends JFrame {
@@ -8,6 +9,9 @@ class panelx extends JFrame {
   JDialog jd = new JDialog();
   JLabel[] options;
   JTextField[] fields;
+  JPanel panelLeft = new JPanel();
+  JPanel panelRight = new JPanel();
+  JPanel panelBottom = new JPanel();
 
   public panelx() {
     this.createDialog();
@@ -15,17 +19,43 @@ class panelx extends JFrame {
 
   private void createDialog() {
     jd = new JDialog();
-    jd.setBounds(100,100,600,500);
+    jd.setBounds(200, 200, 600, 500);
     jd.setMinimumSize(new Dimension(600, 200));
-    jd.setLayout(new GridBagLayout());
+  //  jd.setLayout(new GridBagLayout());
+    panelRight.setLayout(new GridBagLayout());
+    panelLeft.setLayout(new GridBagLayout());
     jd.setUndecorated(true);
+
 
     options = new JLabel[5];
     fields = new JTextField[5];
 
     c = new GridBagConstraints();
+   // c.fill = GridBagConstraints.VERTICAL;
+    Border blackline = BorderFactory.createTitledBorder("Title");
+    panelLeft.setBorder(blackline);
+    panelRight.setBorder(blackline);
+
+    panelLeft.setPreferredSize(new Dimension(300, 170));
+    panelRight.setPreferredSize(new Dimension(300, 170));
+//    panelBottom.setPreferredSize(new Dimension(290, 150));
+
     c.insets = new Insets(10, 10, 5, 5);
     c.fill = GridBagConstraints.HORIZONTAL;
+    c.anchor = GridBagConstraints.PAGE_START;
+
+    panelBottom.setLayout(new GridBagLayout());
+
+    c.gridx =0;
+    c.gridy =0;
+    c.weightx = 0.5;
+    jd.add(panelLeft, BorderLayout.LINE_START);
+    c.gridx =1;
+    c.gridy =0;
+    c.weightx = 0.5;
+    jd.add(panelRight, BorderLayout.LINE_END);
+    c.anchor = GridBagConstraints.PAGE_END;
+    jd.add(panelBottom, BorderLayout.PAGE_END);
 
     createIdBox();
     createMyFareKeyBox();
@@ -35,12 +65,12 @@ class panelx extends JFrame {
     createSaveAndCancel();
     createReadOnly();
     createCreditCardCheckbox();
+    createLabel();
     createAutoMount();
 
     jd.show();
     jd.setResizable(true);
   }
-
 
   private void createComboBox() {
     JLabel comboLabel = new JLabel("Combo");
@@ -48,109 +78,108 @@ class panelx extends JFrame {
     connType.setMaximumRowCount(4);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 0;
-    jd.add(comboLabel, c);
+    panelLeft.add(comboLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
- //   c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 0;
-    jd.add(connType, c);
+    panelLeft.add(connType, c);
+  }
+
+  private void createLabel() {
+    //JLabel idLabel = new JLabel();
+    JTextField idInput = new JTextField();
+    JTextField idInput2 = new JTextField();
+    // c.weightx = 0.250;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 0;
+    c.gridy = 6;
+    idInput2.setVisible(false);
+    panelRight.add(idInput2, c);
+
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 6;
+    idInput.setVisible(false);
+    panelRight.add(idInput, c);
   }
 
   private void createEthernetBox() {
     JLabel dhcpLabel = new JLabel("dhcp");
     JCheckBox dhcp = new JCheckBox("");
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 1;
-    jd.add(dhcpLabel, c);
+    panelLeft.add(dhcpLabel, c);
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 1;
-    jd.add(dhcp, c);
+    panelLeft.add(dhcp, c);
 
-    // IP BOX
     options[0] = new JLabel("ip");
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 2;
-    jd.add(options[0], c);
+    panelLeft.add(options[0], c);
 
     fields[0] = new JTextField();
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 2;
 
-    jd.add(fields[0], c);
+    panelLeft.add(fields[0], c);
 
-    // SubnetMasks
     options[1] = new JLabel("subnet");
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 3;
-    jd.add(options[1], c);
+    panelLeft.add(options[1], c);
 
     fields[1] = new JTextField();
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 3;
-    jd.add(fields[1], c);
+    panelLeft.add(fields[1], c);
 
     // Gateway
     options[2] = new JLabel("gateway");
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 4;
-    jd.add(options[2], c);
+    panelLeft.add(options[2], c);
 
     fields[2] = new JTextField();
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 4;
-    jd.add(fields[2], c);
+    panelLeft.add(fields[2], c);
 
-    // NS1
     options[3] = new JLabel("ns1");
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 5;
-    jd.add(options[3], c);
+    panelLeft.add(options[3], c);
 
     fields[3] = new JTextField();
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 5;
-    jd.add(fields[3], c);
+    panelLeft.add(fields[3], c);
 
-    // NS2
     options[4] = new JLabel("ns2");
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.3;
     c.gridx = 0;
     c.gridy = 6;
-    jd.add(options[4], c);
+    panelLeft.add(options[4], c);
 
     fields[4] = new JTextField();
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.7;
     c.gridx = 1;
     c.gridy = 6;
-    jd.add(fields[4], c);
+    panelLeft.add(fields[4], c);
 
-    // make everything uneditable before Ethernet is chosen
     dhcp.setEnabled(false);
     dhcpLabel.setEnabled(false);
     setEnableOptions(false);
@@ -162,78 +191,62 @@ class panelx extends JFrame {
       fields[i].setEnabled(check);
     }
   }
+
   public void createIdBox() {
     JLabel idLabel = new JLabel("ID");
-    //  idLabel.setSize(150, idLabel.getHeight());
     JTextField idInput = new JTextField();
-    // c.weightx = 0.250;
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 0;
-    jd.add(idLabel, c);
+    panelRight.add(idLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-    //  c.weightx = 0.250;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 0;
     idInput.setText("uduu");
-    jd.add(idInput, c);
+    panelRight.add(idInput, c);
   }
 
   private void createMyFareKeyBox() {
     JLabel myFareKeyLabel = new JLabel("My fare key");
-    //  myFareKeyLabel.setSize(150, myFareKeyLabel.getHeight());
     JTextField myFareKeyInput = new JTextField();
-    //  c.weightx = 0.3;
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 1;
-    jd.add(myFareKeyLabel, c);
+    panelRight.add(myFareKeyLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-    //   c.weightx = 0.7;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 1;
-    jd.add(myFareKeyInput, c);
+    panelRight.add(myFareKeyInput, c);
   }
-
 
   /** Create box to manually select date and time. */
   private void createDateAndTimeBox() {
-    // Date
-
     JLabel dateLabel = new JLabel("date");
-    //dateLabel.setSize(150, dateLabel.getHeight());
     JTextField dateInput = new JTextField();
-   // c.weightx = 0.250;
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 2;
-    jd.add(dateLabel, c);
+    panelRight.add(dateLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
- //   c.weightx = 0.250;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 2;
 
-    jd.add(dateInput, c);
+    panelRight.add(dateInput, c);
 
-    // Time
     JLabel timeLabel = new JLabel("time");
- //   timeLabel.setSize(150, timeLabel.getHeight());
     JTextField timeInput = new JTextField();
-  //  c.weightx = 0.3;
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 3;
-    jd.add(timeLabel, c);
+    panelRight.add(timeLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-   // c.weightx = 0.7;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 3;
-
-    jd.add(timeInput, c);
+    panelRight.add(timeInput, c);
   }
 
   /** Read Only box with checker whether it is selected or not. */
@@ -241,16 +254,14 @@ class panelx extends JFrame {
     JLabel readOnlyLabel = new JLabel("readOnly");
     JCheckBox readOnly = new JCheckBox("");
     c.fill = GridBagConstraints.HORIZONTAL;
- //   c.weightx = 0.3;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 4;
-    jd.add(readOnlyLabel, c);
+    panelRight.add(readOnlyLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 4;
-    jd.add(readOnly, c);
+    panelRight.add(readOnly, c);
   }
 
   /** Credit Card box with checker whether it is selected or not. */
@@ -258,44 +269,40 @@ class panelx extends JFrame {
     JLabel readOnlyLabel = new JLabel("creditCard");
     JCheckBox creditCard = new JCheckBox("");
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.3;
-    c.gridx = 2;
+    c.gridx = 0;
     c.gridy = 5;
-    jd.add(readOnlyLabel, c);
+    panelRight.add(readOnlyLabel, c);
 
     c.fill = GridBagConstraints.HORIZONTAL;
-  //  c.weightx = 0.7;
-    c.gridx = 3;
+    c.gridx = 1;
     c.gridy = 5;
-    jd.add(creditCard, c);
+    panelRight.add(creditCard, c);
   }
 
   /** Save and Cancel buttons on panel */
   private void createSaveAndCancel() {
-    // Save or Cancel buttons
     JButton save = new JButton("save");
+   // save.setPreferredSize(new Dimension(10,10));
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = (int) 1.5;
-    c.gridy = 13;
-    jd.add(save, c);
+    c.gridx = 0;
+    c.gridy = 0;
+    panelBottom.add(save, c);
 
     JButton cancel = new JButton("cancel");
+   // cancel.setPreferredSize(new Dimension(10,10));
     c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 2;
-    c.gridy = 13;
-    jd.add(cancel, c);
+    c.gridx = 1;
+    c.gridy = 0;
+    panelBottom.add(cancel, c);
   }
 
   private void createAutoMount() {
     JButton mount = new JButton("autoMount");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 1;
+    c.fill = GridBagConstraints.CENTER;
     c.gridwidth = 2;
-    c.gridx = (int) 1.5;
-    c.gridy = 14;
-    jd.add(mount, c);
+    c.gridx = 0;
+    c.gridy = 1;
+    panelBottom.add(mount, c);
   }
 
   public static void main(String[] args) {
